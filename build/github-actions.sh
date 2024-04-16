705d56c8ddddba0cdbe969606ae130ac2aa93c81
Nishidha <npanpa23@in.ibm.com>
2024-04-16 16:34:46 +0530

    Update github-actions.sh

--- build/github-actions.sh
+++ build/github-actions.sh
@@ -50,10 +50,11 @@ linux_arm64_create_artifacts() {
 }
 
 linux_ppc64le_create_artifacts() {
-  mkdir linux-ppc64le-built
+  mkdir linux-ppc64le
+  podman images
   ID=$(podman create --platform linux/ppc64le skaji/relocatable-perl)
-  podman cp $ID:/perl-linux-ppc64le.tar.gz linux-ppc64le-built/
-  podman cp $ID:/perl-linux-ppc64le.tar.xz linux-ppc64le-built/
+  podman cp $ID:/perl-linux-ppc64le.tar.gz linux-ppc64le/
+  podman cp $ID:/perl-linux-ppc64le.tar.xz linux-ppc64le/
   podman rm $ID
 }
 
